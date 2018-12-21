7599374d91b967371de61d48d5213cb8bd72c222
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-12-21 17:05:40 +0700

    update

--- Exercise9/main.py
+++ Exercise9/main.py
@@ -1,10 +1,7 @@
-from data_requester import DataRequester
-from user import User
-from excel_manager import ExcelManager
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from login_screen import LoginScreen
 
-dict_users = DataRequester.get_url_response()
-users = list(map(lambda dict_user: User(dict_user), dict_users))
-for user in users:
-	print(user.to_string())
-excel_manager = ExcelManager('Book1.xlsx')
-excel_manager.fill_data_to_first_sheet(users)
+app = QApplication([])
+app.setStyle('Fusion')
+login_screen = LoginScreen()
+app.exec_()
