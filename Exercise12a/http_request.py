b5e22342211487349f4afb306ce80b9dbd47a5a2
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-01-09 12:56:00 +0700

    update

--- Exercise12a/http_request.py
+++ Exercise12a/http_request.py
@@ -9,21 +9,20 @@ class HTTPRequest:
         self.url = url
         self.file_name = 'output.html'
     def get_response(self):
-        with urllib.request.urlopen(self.url) as response:
-            self.file = open(self.file_name,"w+")            
-            self.text_response = response.read().decode("utf-8")
-            self.file.write(self.text_response)            
-            self.file.close()                        
-            print('aaa')
+        response = urllib.request.urlopen(self.url,timeout=10)
+        self.file = open(self.file_name,"w+")            
+        self.text_response = response.read().decode("utf-8")
+        self.file.write(self.text_response)            
+        self.file.close()                        
+        print('aaa')        
+
     def get_objects_from_text(self):
         # self.file = open(self.file_name,'r')
         # self.text_response = self.file.read()
         # self.file.close() 
-        search_usd = re.search( r'usd[\s\t\r]*</th>[\s\t\r]*<td>[\s\t\r]*([\d\,]*)<div>([\d\,]*)<div>([\d\,]*)[\s\t\r]*</td>', self.text_response, re.MULTILINE|re.I)
-        search_usd = re.search( r'usd[\s\t\r]*</th>[\s\t\r]*<td>[\s\t\r]*([\d\,]*)<div>([\d\,]*)<div>([\d\,]*)[\s\t\r]*</td>', self.text_response, re.MULTILINE|re.I)
-        search_usd = re.search( r'usd[\s\t\r]*</th>[\s\t\r]*<td>[\s\t\r]*([\d\,]*)<div>([\d\,]*)<div>([\d\,]*)[\s\t\r]*</td>', self.text_response, re.MULTILINE|re.I)
-        
-        # matchObj = re.match( r'usd[\s\t\r]*', self.text_response, re.I)
-        # for match in matches:
-        #     print ("matchObj.group(1) : ", match.group(1))         
-        print('aa')
+        rows = re.findall( r'usd[\s\t\r]*</th>[\s\t\r]*<td>[\s\t\r]*([\d\,]*)<div>([\d\,]*)<div>([\d\,]*)[\s\t\r]*</td>', self.text_response, re.MULTILINE|re.I)                
+        for row in rows:            
+            muaTM = float(row[0].replace(',','.'))
+            muaCK = float(row[0].replace(',','.'))
+            ban = float(row[0].replace(',','.'))
+            pass                            
